b615200ad96a1010a565780bf601c68c8f343c1b
Kievbuy <kievbuy@gmail.com>
2017-08-16 17:36:00 +0300

    Add creation of new posts

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:blog.db'
 
 class Post < ActiveRecord::Base
+  validates :author, presence: true
+  validates :content, presence: true
 end
 
 class Comment < ActiveRecord::Base
@@ -13,4 +15,22 @@ end
 
 get '/' do
   erb 'Hello, world!'
+end
+
+get '/new' do
+  @p = Post.new
+  erb :new
+end
+
+
+# ==== POST ====
+
+post '/new' do
+  @p = Post.new params[:post]
+  if @p.save
+    erb "<h2>Спасибо. Ваш пост добавлен.</h2>"
+  else
+    @error = @p.errors.full_messages.first
+    erb :new
+  end
 end
