9668b450dea3f84554bf3324a8dac99bc18d582c
Kievbuy <kievbuy@gmail.com>
2017-08-16 17:09:36 +0300

    Install gems, add Gemfile, add Rakefile

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, 'sqlite3:blog.db'
+
+class Post < ActiveRecord::Base
+end
 
 get '/' do
   erb 'Hello, world!'
